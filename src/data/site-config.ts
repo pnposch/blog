c5f34190a6bf71eff78d8df43ebd64c7e86c648a
PN Posch <pn@posch.net>
2025-04-24 13:51:54 +0200

    o

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -39,14 +39,14 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    website: 'https://example.com',
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
-    image: {
-        src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
-    },
+    website: 'https://pn.posch.org',
+    title: 'PN Posch',
+    subtitle: 'A Blog',
+    description: 'impressions from the road',
+    /*     image: {
+            src: '/dante-preview.jpg',
+            alt: 'Dante - Astro.js and Tailwind CSS theme'
+        }, */
     headerNavLinks: [
         {
             text: 'Home',
@@ -61,61 +61,52 @@ const siteConfig: SiteConfig = {
             href: '/blog'
         },
         {
-            text: 'Tags',
-            href: '/tags'
-        }
-    ],
-    footerNavLinks: [
-        {
             text: 'About',
             href: '/about'
         },
+        // {
+        //     text: 'Tags',
+        //     href: '/tags'
+        // }
+    ],
+    footerNavLinks: [
+
         {
             text: 'Contact',
             href: '/contact'
         },
         {
-            text: 'Terms',
+            text: 'Impressum & Terms',
             href: '/terms'
         },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
+
     ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
+            text: 'LinkedIn',
+            href: 'https://www.linkedin.com/pnposch'
         },
-        {
-            text: 'Instagram',
-            href: 'https://instagram.com/'
-        },
-        {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
-        }
+
     ],
-    hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
-        image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
-        },
-        actions: [
-            {
-                text: 'Get in Touch',
-                href: '/contact'
-            }
-        ]
-    },
-    subscribe: {
-        title: 'Subscribe to Dante Newsletter',
-        text: 'One update per week. All the latest posts directly in your inbox.',
-        formUrl: '#'
-    },
+    /*     hero: {
+            title: 'Hi There & Welcome to My Corner of the Web!',
+            text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+            image: {
+                src: '/hero.jpeg',
+                alt: 'A person sitting at a desk in front of a computer'
+            },
+            actions: [
+                {
+                    text: 'Get in Touch',
+                    href: '/contact'
+                }
+            ]
+        }, */
+    /*     subscribe: {
+            title: 'Subscribe to Dante Newsletter',
+            text: 'One update per week. All the latest posts directly in your inbox.',
+            formUrl: '#'
+        }, */
     postsPerPage: 8,
     projectsPerPage: 8
 };
