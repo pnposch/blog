bcab485a0d17d6bacc83965d86da31a814f0f484
PN Posch <pn@posch.net>
2025-06-11 12:19:18 +0200

    adding astro-public and moving public

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import siteConfig from './src/data/site-config';
-
+import publicDir from 'astro-public';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +11,5 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
     },
-    integrations: [mdx(), sitemap()]
+    integrations: [mdx(), sitemap(),   publicDir('src/content/public')],
 });
